6a7168b723282e1650139914e0d9660b0bb0d9d5
Harish8801 <92293513+Harish8801@users.noreply.github.com>
2022-06-17 11:58:18 +0530

    final

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css';
+import Header from './components/header/header';
+import Footer from "./components/footer/Footer"
+import Landingpage from './components/landingpage/landingpage';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Landingpage/>
+      <Footer/>
+      
+
+    </div>
+  );
+}
+
+export default App;
